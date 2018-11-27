03c2e9936d795da534458c1e3ffa44bb6a9b6a12
Alexey <menamet31@gmail.com>
2018-11-27 19:08:19 +0000

    experiment function added

--- train_test.py
+++ train_test.py
@@ -11,26 +11,17 @@ import matplotlib.pyplot as plt
 
 from graph_gen import SinGraph
 
-
-LEARNING_RATE = 0.005
-BATCH_SIZE = 100
-NUM_EPOCHS = 250
-SEQUENCE_LENGTH = 75
-HIDDEN_NEURONS=8
-NUM_LAYERS = 1
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-TYPE = nn.RNN # nn.GRU or nn.GRU
 
 
 class MyRNN(nn.Module):
-    def __init__(self, hidden_size, num_layers=NUM_LAYERS, dropout_rate=0.2):
+    def __init__(self, rnn_type, hidden_size, num_layers=1, dropout_rate=0.2):
         super(MyRNN, self).__init__()
 
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         #
-        self.rnn = TYPE(input_size=1, hidden_size=hidden_size, num_layers=self.num_layers, dropout=dropout_rate)
+        self.rnn = rnn_type(input_size=1, hidden_size=hidden_size, num_layers=self.num_layers, dropout=dropout_rate)
         self.out = nn.Linear(hidden_size, 1)
 
     def init_hidden_state(self, batch_size):
@@ -64,13 +55,13 @@ class MyDataset(Dataset):
             return np.array(input, dtype='float32'), np.array(output, dtype='float32')
 
 
-def train_model(model, dataloader, loss_function, optimizer, epochs):
+def train_model(model, dataloader, loss_function, optimizer, batch_size, epochs):
     model.train()
     loss_all = []
 
-    for epoch in range(epochs):
+    for epoch in range(1,epochs+1):
         for x_batch, y_batch in dataloader:
-            model.init_hidden_state(BATCH_SIZE)
+            model.init_hidden_state(batch_size)
             optimizer.zero_grad()
 
             x_batch = x_batch[:,:,np.newaxis].permute([1, 0, 2])
@@ -87,7 +78,8 @@ def train_model(model, dataloader, loss_function, optimizer, epochs):
 
         loss_all.append(loss.cpu().data.numpy())
 
-        print('Training loss for epoch {} : '.format(epoch), loss.cpu().data.numpy())
+        if epoch % 10 == 0 or epoch == 1:
+            print('Training loss for epoch {} : '.format(epoch), loss.cpu().data.numpy())
 
 
 def test_model(model, dataloader, init_sequence_length):
@@ -122,23 +114,32 @@ def test_model(model, dataloader, init_sequence_length):
 
     return final_outputs
 
-def main():
-    graph = SinGraph()
 
-    points = graph.generate()
-    split = points
+def experiment(graph, rnn_type, seq_leng, batch_size, hidden_n, lyers_n, learning_rate, epochs_n):
+    split = graph.generate()
+    dataloder = DataLoader(MyDataset(split, seq_leng), batch_size=batch_size, shuffle=True, drop_last=True)
 
-    print(len(points))
+    rnn = MyRNN(rnn_type, hidden_size=hidden_n, num_layers=lyers_n).to(DEVICE)
 
-    dataloder = DataLoader(MyDataset(split, SEQUENCE_LENGTH), batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
-
-    rnn = MyRNN(hidden_size=HIDDEN_NEURONS, num_layers=1).to(DEVICE)
-
-    optimizer = torch.optim.Adam(rnn.parameters(), lr=LEARNING_RATE)
+    optimizer = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
     loss_function = nn.MSELoss()
 
-    train_model(rnn, dataloader=dataloder, loss_function=loss_function, optimizer=optimizer, epochs=NUM_EPOCHS)
-    test_model(rnn, dataloder, init_sequence_length=SEQUENCE_LENGTH)
+    train_model(rnn, dataloader=dataloder, loss_function=loss_function, optimizer=optimizer, batch_size = batch_size, epochs=epochs_n)
+    test_model(rnn, dataloder, init_sequence_length=seq_leng)
+
+def main():
+
+    LEARNING_RATE = 0.005
+    BATCH_SIZE = 100
+    NUM_EPOCHS = 250
+    SEQUENCE_LENGTH = 75
+    HIDDEN_NEURONS=8
+    NUM_LAYERS = 1
+
+    experiment(SinGraph(), nn.RNN, SEQUENCE_LENGTH, BATCH_SIZE, HIDDEN_NEURONS, NUM_LAYERS, LEARNING_RATE, NUM_EPOCHS)
+    experiment(SinGraph(), nn.GRU, SEQUENCE_LENGTH, BATCH_SIZE, HIDDEN_NEURONS, NUM_LAYERS, LEARNING_RATE, NUM_EPOCHS)
+
+
 
 
 if __name__ == '__main__':
